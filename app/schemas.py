2e73cb1b56142eaa21e009375d1a13a114e5bf0a
Darshan Vikram Shivadas <darshanvikramshivadas@gmail.com>
2025-05-10 02:12:23 +0530

    Implemented User Registration Path

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
 class PostBase(BaseModel):
     title: str
@@ -6,4 +7,23 @@ class PostBase(BaseModel):
     published: bool = True
 
 class PostCreate(PostBase):
-    pass    
+    pass
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
