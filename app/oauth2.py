fc9b9b33ade9c94bd26918434a92126f6e2297f8
Darshan Vikram Shivadas <darshanvikramshivadas@gmail.com>
2025-06-10 02:57:56 +0530

    Fetch user in protected route

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,8 +1,9 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
-from . import schemas
+from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
@@ -28,7 +29,7 @@ def verify_access_token(token: str, credentials_exception):
         id: str = payload.get("user_id")
         if id is None:
             raise credentials_exception
-        token_data = schemas.TokenData(id=id)
+        token_data = schemas.TokenData(id=str(id))
     except JWTError as e:
         print(f"JWTError: {str(e)}")  # Debug expired token
         raise credentials_exception
@@ -36,7 +37,11 @@ def verify_access_token(token: str, credentials_exception):
     return token_data
 
     
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code= status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
 
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+
+    return user
