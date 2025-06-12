db5457fa01b84c241bd89a56ebc8fa719d6262cc
Darshan Vikram Shivadas <darshanvikramshivadas@gmail.com>
2025-06-13 01:38:00 +0530

    Add query parameters

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, TIMESTAMP, text
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
 class Post(Base):
@@ -11,6 +14,8 @@ class Post(Base):
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
     owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    owner = relationship("User")
 
 class User(Base):
     __tablename__ = "users"
